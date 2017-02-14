7c26b7d4a1b151521e3bcd2582d064317a2947a0
Jeff <rustyangel@gmail.com>
2017-02-14 15:31:49 -0700

    👕 Update eslint config format

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'root': true,
+  'extends': 'airbnb-base',
+  'globals': {
+    'window': true,
+    'document': true,
+    'jQuery': false,
+    '$': true,
+    'Foundation': true
+  },
+  'rules': {
+    'max-len': ['error', 120]
+  }
+}
