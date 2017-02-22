cbc47eb793e9d0b5bd3e018f9c47214083bcd158
Bryan Hyshka <bryan@hyshka.com>
2017-02-22 09:32:43 -0700

    add js module template

---
+++ static/js/modules/_module-template.js
@@ -0,0 +1,10 @@
+// libraries
+// const Rellax = require('rellax/rellax');
+
+function init() {
+  console.log('module-template.js init'); // eslint-disable-line
+
+  // your code
+}
+
+module.exports = init;
