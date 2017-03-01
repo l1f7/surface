9241b399f0f6e098b3bab9f1021973bc967a47ef
Jeff <rustyangel@gmail.com>
2017-03-01 10:23:35 -0700

    📝 Update comment documentation

--- static/js/App.js
+++ static/js/App.js
@@ -1,80 +1,54 @@
 require('jquery'); // eslint-disable-line no-unresolved
 
 ((function App(window, $) {
-  // Vendor Libraries
-  // This also ensures that these libraries are bundled specifically
-  // into this main App.js file instead of being included multiple
-  // times into the children chunks
-  // require('PubSub/src/pubsub');
+  /**
+   * Vendor libraries.
+   *
+   * This also ensures that these libraries are bundled specifically
+   * into this main App.js file instead of being included multiple
+   * times into the children chunks.
+   */
+  // const debounce = require('throttle-debounce/debounce');
+  // const throttle = require('throttle-debounce/throttle');
 
-  //
-  // Foundation Core & Utilities
-  //
-  // require('what-input/what-input.js');
-  // require('foundation-sites/js/foundation.core.js');
-  // require('foundation-sites/js/foundation.util.mediaQuery.js');
-  // require('foundation-sites/js/foundation.util.timerAndImageLoader.js');
-  // require('foundation-sites/js/foundation.util.triggers.js');
-  // require('foundation-sites/js/foundation.util.motion.js');
-  // require('foundation-sites/js/foundation.util.box.js');
-  // require('foundation-sites/js/foundation.util.keyboard.js');
-  // require('foundation-sites/js/foundation.util.nest.js');
-  // require('foundation-sites/js/foundation.util.touch.js');
-
-  //
-  // Foundation Modules
-  //
-  // require('foundation-sites/js/foundation.abide.js');
-  // require('foundation-sites/js/foundation.reveal.js');
-  // require('foundation-sites/js/foundation.accordion.js');
-  // require('foundation-sites/js/foundation.accordionMenu.js');
-  // require('foundation-sites/js/foundation.drilldown.js');
-  // require('foundation-sites/js/foundation.dropdown.js');
-  // require('foundation-sites/js/foundation.dropdownMenu.js');
-  // require('foundation-sites/js/foundation.equalizer.js');
-  // require('foundation-sites/js/foundation.interchange.js');
-  // require('foundation-sites/js/foundation.magellan.js');
-  // require('foundation-sites/js/foundation.offcanvas.js');
-  // require('foundation-sites/js/foundation.orbit.js');
-  // require('foundation-sites/js/foundation.responsiveMenu.js');
-  // require('foundation-sites/js/foundation.responsiveToggle.js');
-  // require('foundation-sites/js/foundation.slider.js');
-  // require('foundation-sites/js/foundation.sticky.js');
-  // require('foundation-sites/js/foundation.tabs.js');
-  // require('foundation-sites/js/foundation.toggler.js');
-  // require('foundation-sites/js/foundation.tooltip.js');
-
-  //
-  // LIFT Base Modules
-  //
-  // require('./base/csrf')(); // Needed for Ajax
-  // require('./base/forms')(); // Needed for Ajax
+  /**
+   * Lift default modules.
+   */
+  // require('./base/ajaxForms')(); // Needed for Ajax
   // require('./base/alerts')();
-  // require('./base/jquery.throttle-debounce')();
+  require('./base/csrf')(); // Needed for Ajax
   // require('./base/prettyImageUpload')();
   // require('./base/progressiveImages')();
 
 
   const LIFT = {
-    // common gets called for every single page/view which means
-    // global elements should be contained in here
-    // ie. global navigation
+    /**
+     * Base view.
+     *
+     * Called on every page/view. Add global components here (eg: navigation
+     * modules, etc.).
+     */
     common: {
+
+      /**
+       * `init` is the action each view executes by default.
+       */
       init() {
-        /*eslint-disable */
-        console.log('%cMADE BY LIFT | liftinteractive.com',
-          `background: white; color: #333; border: 1px solid #333;
-           line-height: 30px; padding: 5px 10px;`);
-        /*eslint-enable */
+        require('./base/hello')();
 
         $(window).trigger('el.init');
-        // $(document).foundation();
       },
     },
   };
 
 
   const UTIL = {
+    /**
+     * Executes the defined view function.
+     *
+     * @param {Object}   model         - The current view.
+     * @param {Function} [action=init] - The function to execute.
+     */
     exec(model, action) {
       const modelAction = (action === undefined) ? 'init' : action;
 
@@ -82,24 +56,30 @@ require('jquery'); // eslint-disable-line no-unresolved
         LIFT[model][modelAction]();
       }
     },
+
+    /**
+     * Gets the view and action (if defined) from the main DOM container.
+     *
+     * @example
+     *     <div class=".l-body" data-model="home" data-action="init">
+     */
     init() {
-      // the following looks for an element with a class "body"
-      // and then pulls the value of it's data-model and data-action
-      // attributes (if they exist, which they should):
-      // ie.: <div class="mainblock" data-model="myModel">
       const $sitewrap = $('.l-body');
       const model = $sitewrap.data('model');
       const action = $sitewrap.data('action');
 
-      UTIL.exec('common');      // calls LIFT.common.init()
-      UTIL.exec(model);         // calls LIFT.model.init()
+      // Calls LIFT.common.init()
+      UTIL.exec('common');
+      // Calls LIFT.model.init()
+      UTIL.exec(model);
 
       if (action !== undefined) {
-        UTIL.exec(model, action); // calls LIFT.model.action()
+        // Calls LIFT.model.action()
+        UTIL.exec(model, action);
       }
     },
   };
 
-  // DOM ready, let's DO DIS!
+  /** Lift-off in T-minus DOMReady. */
   $(document).ready(UTIL.init);
 })(window, jQuery));
