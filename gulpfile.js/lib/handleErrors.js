79fa4e14485bf1aed5ec7feb04ac0c97019a6f92
Jeff <rustyangel@gmail.com>
2017-03-02 14:47:50 -0700

    👕 Document & cleanup build modules

--- gulpfile.js/lib/handleErrors.js
+++ gulpfile.js/lib/handleErrors.js
@@ -1,7 +1,21 @@
-var notify = require("gulp-notify");
+/**
+ * Error notification.
+ *
+ * Display a popup notification when a Gulp task returns an error. Also
+ * prevents running tasks from stopping (just won't output anything).
+ *
+ * @module surface/handleErrors
+ * @param  {Object} errorObject
+ * @since  1.0.0
+ */
+/* eslint-disable import/no-extraneous-dependencies */
 
-module.exports = function(errorObject, callback) {
+const notify = require('gulp-notify');
+
+
+module.exports = (errorObject, callback) => {
   notify.onError(errorObject.toString().split(': ').join(':\n')).apply(this, arguments);
+
   // Keep gulp from hanging on this task
   if (typeof this.emit === 'function') this.emit('end');
 };
